e84ff23a148992e9beaa05aa3f141b1ceeab2553
Parker Mathis <parkerjmathis@gmail.com>
2022-11-25 13:09:51 -0800

    Added pseudo code

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/////////////////////////////////////////////////////////////////////////
+// PSEUDO CODE //////////////////////////////////////////////////////////
+// 1. Install inquirer
+// 2. Set up the inputs for the README
+///// What is included in the README?
+//////// Title of my project
+//////// Sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+////////// Table of Contents
+/////////////// Example "[License](#license)" ....... # license
+////////// Installation
+/////////////// Instructions, install NPM / inquirer?
+////////// Usage
+////////// Contributing
+////////// Tests
+////////// Questions
+/////////////// Should prompt the user for github username. Use gibhub api and return profile link?
+/////////////// Should prompt the user for email address, then include instructions on how to reach out with additional questions
+
 const fs = require("fs");
 const inquirer = require("inquirer");
 
