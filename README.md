7811e9f3fa495fd3337a4e5cc7f0d4f60cc27497
Parker Mathis <parkerjmathis@gmail.com>
2022-11-28 17:19:47 -0800

    Refactor renderLicenseBade with switch statement

---
+++ README.md
@@ -0,0 +1,38 @@
+
+  # a
+  
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+
+  ## Description
+  b
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  c
+  
+  ## Usage
+  undefined
+  
+  ## License
+  This project is licensed under the terms of the Apache License 2.0.
+
+  ## Contributing
+  d
+  
+  ## Tests
+  e
+  
+  ## Questions
+  f
+  <br />
+  [LINK TO USER ON GITHUB](https://github.com/f)
+  <br />
+  g
+      
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,20 +3,21 @@
 
 // TRY OUT A SWITCH STATEMENT
 function renderLicenseBadge(answers) {
-  if (answers.inputLicense === "Apache License 2.0") {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)`;
-  } else if (answers.inputLicense === "GNU General Public License v3.0") {
-    return `[![License](https://img.shields.io/badge/License-GNU_General_Public_License_v3.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
-  } else if (answers.inputLicense === "MIT License") {
-    return `[![License](https://img.shields.io/badge/License-MIT_License-blue.svg)](https://mit-license.org/)`;
-  } else if (answers.inputLicense === "BSD 2-Clause 'Simplified' License") {
-    return `[![License](https://img.shields.io/badge/License-BSD_2--Clause_Simplified_License-blue.svg)](https://choosealicense.com/licenses/bsd-2-clause/)`;
-  } else if (
-    answers.inputLicense === "BSD 3-Clause 'New' or 'Revised' License"
-  ) {
-    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause_New_or_Revised_License-blue.svg)](https://choosealicense.com/licenses/bsd-3-clause/)`;
-  } else {
-    return ``;
+  const license = answers.inputLicense;
+
+  switch (license) {
+    case "Apache License 2.0":
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)`;
+    case "GNU General Public License v3.0":
+      return `[![License](https://img.shields.io/badge/License-GNU_General_Public_License_v3.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
+    case "MIT License":
+      return `[![License](https://img.shields.io/badge/License-MIT_License-blue.svg)](https://mit-license.org/)`;
+    case "BSD 2-Clause 'Simplified' License":
+      return `[![License](https://img.shields.io/badge/License-BSD_2--Clause_Simplified_License-blue.svg)](https://choosealicense.com/licenses/bsd-2-clause/)`;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause_New_or_Revised_License-blue.svg)](https://choosealicense.com/licenses/bsd-3-clause/)`;
+    default:
+      return ``;
   }
 }
 
