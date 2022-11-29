feac521d3707e13029e2505fce2471f49231832b
Parker Mathis <parkerjmathis@gmail.com>
2022-11-28 17:56:49 -0800

    Added final licenses to inquirer prompts with URLs commented out

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ const questions = [
       "Boost Software License 1.0",
       "Creative Commons Zero v1.0 Universal License",
       "Eclipse Public License 2.0",
+      // NEW
+      "GNU Affero General Public License v3.0",
+      // https://www.gnu.org/licenses/agpl-3.0.en.html
+      "GNU General Public License v2.0",
+      // https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+      "GNU Lesser General Public License v2.1",
+      // https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
+      "Mozilla Public License 2.0",
+      // https://www.mozilla.org/en-US/MPL/2.0/
+      "The Unlicense",
+      // https://unlicense.org/
       "None",
     ],
   },
